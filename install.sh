7b493368d16cc4b75b00c35b9008218042de22ff
rwrtx <152061298+rwrtx@users.noreply.github.com>
2024-05-03 20:17:37 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,13 +11,13 @@ apt install certbot -y
 clear
 echo -e "Installasi CerBOT Selesai"
 echo -e "Memulai Installasi WARP Cloudflare"
-wget --no-check-certificate "https://raw.githubusercontent.com/wrtx/panelwarp/main/warp.sh" -O /usr/bin/warp && chmod +x /usr/bin/warp
+wget --no-check-certificate "https://raw.githubusercontent.com/rwrtx/panelwarp/main/warp.sh" -O /usr/bin/warp && chmod +x /usr/bin/warp
 clear
 echo -e "Installasi WARP selesai"
 echo -e "Memulai installasi Auto Reboot VPS"
 echo -e "Downloading Binaries Files"
 sleep 2
-wget --no-check-certificate "https://raw.githubusercontent.com/wrtx/panelwarp/main/auto-reboot.sh" -O /root/auto-reboot.sh && chmod +x /root/auto-reboot.sh
+wget --no-check-certificate "https://raw.githubusercontent.com/rwrtx/panelwarp/main/auto-reboot.sh" -O /root/auto-reboot.sh && chmod +x /root/auto-reboot.sh
 clear
 echo -e "setting Auto Reboot at 00.00"
 sleep 3
@@ -27,7 +27,7 @@ chmod +x /etc/cron.d/auto-reboot && chmod +x /root/auto-reboot.sh
 clear
 echo -e "Auto Reboot VPS installed"
 echo -e "Memulai Installasi Speedtest"
-wget --no-check-certificate "https://raw.githubusercontent.com/wrtx/panelwarp/main/speedtest" -O /usr/bin/speedtest && chmod +x /usr/bin/speedtest
+wget --no-check-certificate "https://raw.githubusercontent.com/rwrtx/panelwarp/main/speedtest" -O /usr/bin/speedtest && chmod +x /usr/bin/speedtest
 clear
 echo -e "Installasi Speedtest Selesai"
 sleep 3
